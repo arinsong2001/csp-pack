7fba06cea8b946b550b172f0e547bb46dc14ee73
ArinSong <arinsong2001@qq.com>
2026-04-07 22:36:02 +0800

    2026-04-07

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,12 +17,17 @@ vim.g.mapleader = " "
 
 local cmd = "(trap 'rm -f a.out' INT && g++ -std=c++23 -O2 % && ./a.out; rm -f a.out)"
 
-vim.keymap.set("n", "<C-'>", ":bel ter<CR>i", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-t>", ":bel ter<CR>i", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>t", ":bel ter<CR>i", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>r", ":w<CR>:bel ter " .. cmd .. "<CR>i", { noremap = true, silent = true })
 
 if vim.g.vscode then
   local vscode = require("vscode")
+
+  vim.keymap.set("n", "<C-t>", function()
+    vscode.action("workbench.action.terminal.toggleTerminal")
+  end, { noremap = true, silent = true })
+
   vim.keymap.set("n", "<leader>t", function()
     vscode.action("workbench.action.terminal.toggleTerminal")
   end, { noremap = true, silent = true })
