6a54937dff38d301054f140dddd51eb08e365cfa
ArinSong <arinsong2001@qq.com>
2026-04-12 19:09:59 +0800

    2026-04-12

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,29 +15,55 @@ vim.cmd("syn on")
 
 vim.g.mapleader = " "
 
-local cmd = "(trap 'rm -f a.out' INT && g++ -std=c++23 -O2 % && ./a.out; rm -f a.out)"
-
 vim.keymap.set("n", "<leader>t", ":bel ter<CR>i", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>r", ":w<CR>:bel ter " .. cmd .. "<CR>i", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>r", function()
+  local ft = vim.bo.filetype
+
+  local cmd
+  if ft == "cpp" then
+    cmd = "(trap 'rm -f a.out' INT && g++ -std=c++23 -O2 % && ./a.out; rm -f a.out)"
+  elseif ft == "python" then
+    cmd = "python3 %"
+  else
+    return
+  end
+
+  vim.cmd("w")
+  vim.cmd("bel ter " .. cmd)
+  vim.cmd("startinsert")
+end, { noremap = true, silent = true })
 
 if vim.g.vscode then
   local vscode = require("vscode")
+
+  vim.defer_fn(function()
+    vim.cmd("se rnu")
+  end, 200)
 
   vim.keymap.set("n", "<leader>t", function()
     vscode.action("workbench.action.terminal.toggleTerminal")
   end, { noremap = true, silent = true })
 
   vim.keymap.set("n", "<leader>r", function()
-    local file = vim.fn.expand("%:p")
-    local vcmd = "(trap 'rm -f a.out' INT && g++ -std=c++23 -O2 " .. file .. " && ./a.out; rm -f a.out)"
+    local ft = vim.bo.filetype
+    local f= vim.fn.expand("%:p")
+
+    local vcmd
+    if ft == "cpp" then
+      vcmd = "(trap 'rm -f a.out' INT && g++ -std=c++23 -O2 " .. f.. " && ./a.out; rm -f a.out)"
+    elseif ft == "python" then
+      vcmd = "python3 " .. f
+    else
+      return
+    end
 
     vscode.action("workbench.action.files.save")
     vscode.action("workbench.action.terminal.focus")
 
     vim.defer_fn(function()
-      vscode.action("workbench.action.terminal.sendSequence", { args = { text = vcmd .. "\r" } })
+      vscode.action("workbench.action.terminal.sendSequence", { args = { text = vcmd .. "\r"} })
     end, 200)
-
   end, { noremap = true, silent = true })
 end
 
